f9c54854dd59cd8bf4dddf37fcc05b3a951a98f4
Ben Smithett <bensmithett@users.noreply.github.com>
2024-01-21 22:43:39 +1100

    Demo for github pages

--- modules/PNGExporter.js
+++
@@ -1,9 +0,0 @@
-export class PNGExporter {
-  constructor(diagram, joint) {
-    document.querySelector('#export').addEventListener('click', () => {
-      diagram.exportCurrentDiagramToPNG(true, false, (dataURI) => {
-        joint.util.downloadDataUri(dataURI, `${diagram.getCurrentView().key}.png`)
-      })
-    })
-  }
-}
--- modules/WindowResizeHandler.js
+++
@@ -1,9 +0,0 @@
-export class WindowResizeHandler {
-  constructor(diagram) {
-    const observer = new ResizeObserver(() => {
-      diagram.resize()
-      diagram.zoomToWidthOrHeight()
-    })
-    observer.observe(document.body)
-  }
-}
