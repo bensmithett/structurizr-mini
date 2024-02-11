d71988709753267c2ee22139d0ddd576404b8a0a
Ben Smithett <bensmithett@users.noreply.github.com>
2024-02-11 14:40:10 +1100

    Formatting

--- modules/Router.js
+++ modules/Router.js
@@ -22,7 +22,7 @@ export class Router {
   handleElementDoubleClick = (event, elementId) => {
     const element = structurizr.workspace.findElementById(elementId)
     
-    if(element.url) window.open(element.url, '_blank');
+    if (element.url) window.open(element.url, '_blank')
 
     switch (element.type) {
       case structurizr.constants.SOFTWARE_SYSTEM_ELEMENT_TYPE:
